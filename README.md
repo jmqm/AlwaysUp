f2676538256a0d48ac796843ed721fd3cc7133af
jmqm <jmqm@outlook.com>
2022-01-03 17:59:09 -0600

    Improvements 1 (#1)
    - Docker stuff added + created.
      - Workflow
      - `Dockerfile`
      - `.dockerignore`
      - `requirements.txt`
    - Created README.
    - Script now run 24/7 (initially envisioned to run 24/7), no longer one-time.
    - Timestamps on prints.
    - More prints.
    - Other minor improvements/changes.

---
+++ README.md
@@ -0,0 +1,31 @@
+Keep files alive, using AnonFiles.com.
+
+## About this project.
+Using AnonFiles.com, files are uploaded and a corresponding text file is created with the link to the file. If a link goes down, the file is uploaded again and its text file's link is changed.
+
+## docker-compose
+```
+services:
+  alwaysup:
+    image: jmqm/alwaysup:latest
+    container_name: alwaysup
+    network_mode: none
+    volumes:
+      - /host_files:/files:ro # Read-only
+      - /host_links:/links
+
+      - /etc/localtime:/etc/localtime:ro # Container uses date and time from host.
+    environment:
+      - DELAY_MINUTES=60
+      # - TZ=Asia/Tokyo # Alternative to mounting /etc/localtime.
+```
+
+## Volumes _(permission required)_
+`/files` _(read)_- Files to upload. All files in this directory will be considered.
+
+`/links` _(write)_ - Where to store links to.
+
+### Other info
+Thought to create this software because it seemed fun, and in no way do I support the distribution of illegal material. Do not abuse this software.
+
+This project is not associated with AnonFiles.com aside from using its services for file upload.
--- alwaysup.py
+++ alwaysup.py
@@ -1,64 +1,82 @@
 import os
 import json
 import requests
+from time import sleep
 from datetime import datetime
 
+DELAY = int(os.environ["DELAY_MINUTES"])
 API_URL = "https://api.anonfiles.com"
 
-#region Helper Methods
+# region Functions
 
 def isURLValid(file_url: str):
     """Checks if a file is still available for download."""
-    try:
-        file_id = file_url.split("/")[-1]
-
-        response = requests.get(f"{API_URL}/v2/file/{file_id}/info")
-        return json.loads(response.content)["status"]
-    except:
+    if not file_url:
         return False
+
+    file_id = file_url.split("/")[-1]
+    response = requests.get(f"{API_URL}/v2/file/{file_id}/info")
+    return json.loads(response.content)["status"]
+
 
 def uploadFile(file_location: str):
     """Uploads a file and returns its short url."""
     file = open(file_location, "rb")
-
-    response = requests.post(f"{API_URL}/upload", files = { "file": file })
+    response = requests.post(f"{API_URL}/upload", files={"file": file})
     responseJson = json.loads(response.content)
 
     # If not uploaded properly, return an empty string.
     if not responseJson["status"]:
         return ""
-    
+
     return responseJson["data"]["file"]["url"]["short"]
+
 
 def timestamp():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-#endregion
 
-for file in os.listdir("/files"):
-    file_location = f"/files/{file}"
-    url_location = f"/links/{file}.txt"
+def printf(file: str, message: str):
+    print(f"[{timestamp()}] ", end="")
+    if file: print(f"{file} - ", end="")
+    print(message)
 
-    # If the file is not a file, skip it.
-    if not os.path.isfile(file_location):
-        continue
 
-    # If url file exists and is valid, skip it; continue otherwise.
-    if os.path.exists(url_location):
-        url = open(url_location).read()
+def run():
+    for file in os.listdir("/files"):
+        file_location = f"/files/{file}"
+        url_location = f"/links/{file}.txt"
 
-        if isURLValid(url):
-            print(f"[{timestamp()}] {file} - Link still valid, skipping...")
+        if not os.path.isfile(file_location):
             continue
 
-    # Upload file.
-    response = uploadFile(file_location)
+        # If url file exists and is valid, skip it; continue otherwise.
+        if os.path.exists(url_location):
+            url = open(url_location).read()
 
-    if response:
-        open(url_location, "w+").write(response)
+            if isURLValid(url):
+                printf(file, "Link still valid, skipping...")
+                continue
 
-        print(f"[{timestamp()}] {file} - File uploaded successfully, link file created.")
-    else:
-        print(f"[{timestamp()}] {file} - ERROR Could not upload file.")
+            printf(file, "Link down, uploading...")
 
-print("Finished processing all files.")
+        # Upload file.
+        response = uploadFile(file_location)
+
+        if response:
+            writer = open(url_location, "w+")
+            writer.write(response)
+
+            printf(file, "File uploaded successfully, link file created.")
+        else:
+            printf(file, "ERROR Could not upload file.")
+
+# endregion
+
+printf("", "Started")
+
+while True:
+    run()
+
+    printf("", f"Sleeping for {DELAY} minutes.")
+    sleep(DELAY * 60)
